59861c2d01ce4afc7a79f75c372eee841aec46cc
nobulin515 <s1f102400875@iniad.org>
2024-05-16 21:04:52 +0900

    枠組み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,37 @@ import { useState } from 'react';
 import styles from './index.module.css';
 
 const Home = () => {
-  const [samplePos,setSamplePos]=useState(0)
-  console.log('sample',samplePos)
-
+  const [userInputs, setUserInput] = useState([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const clickHandler = (x: number, y: number) => {
+    console.log(x, y);
+  };
   return (
-    <div className={styles.container} >
-      <div className={styles.smpleStyle} style={{backgroundPosition: `${samplePos*-30}px 0px`}}> </div>
-  <button onClick={()=>setSamplePos(p=>(p+1)%14)}>sample</button>
+    <div className={styles.container}>
+      <div className={styles.box}>
+        <div className={styles.board}>
+          {userInputs.map((row, y) =>
+            row.map((color, x) => (
+              <div
+                className={styles.cellStyle}
+                key={`${x}-${y}`}
+                onClick={() => clickHandler(x, y)}
+              />
+            )),
+          )}
+        </div>
+      </div>
     </div>
   );
 };
-
 
 export default Home;
