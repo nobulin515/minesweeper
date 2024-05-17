66a7b113172a3650c3baad7ef35d7e31bccda73b
nobulin515 <s1f102400875@iniad.org>
2024-05-17 20:55:23 +0900

    次はボムのランダム生成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,32 @@
 import { useState } from 'react';
 import styles from './index.module.css';
+const board = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+const creatBoard = (userInputs: number[][], bombMap: number[][]) => {
+  for (let i = 0; i < 9; i++) {
+    for (let o = 0; o < 9; o++) {
+      if (userInputs[i][o] === 0) {
+        board[i][o] = bombMap[i][o];
+      } else if (userInputs[i][o] === -1) {
+        board[i][o] = userInputs[i][o];
+      }
+    }
+  }
+  console.log(board);
+};
 
 const Home = () => {
-  const [userInputs, setUserInput] = useState([
+  const [userInputs, setUserInputs] = useState([
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -13,17 +37,34 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
+  const [bombMap, setBombMaps] = useState([
+    [11, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
   const clickHandler = (x: number, y: number) => {
+    const newBombMap = structuredClone(bombMap);
     console.log(x, y);
+    creatBoard(userInputs, bombMap);
+    // console.log(board);
+    setBombMaps(newBombMap);
   };
+  console.log(board);
   return (
     <div className={styles.container}>
       <div className={styles.box}>
         <div className={styles.board}>
-          {userInputs.map((row, y) =>
+          {board.map((row, y) =>
             row.map((color, x) => (
               <div
-                className={styles.cellStyle}
+                className={styles.block}
+                style={{ backgroundPosition: `${(board[y][x] - 1) * -30}px 0px` }}
                 key={`${x}-${y}`}
                 onClick={() => clickHandler(x, y)}
               />
